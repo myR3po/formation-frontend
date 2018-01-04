a67589912c5621eff7accbfd0baa3aba9bbeddb0
myR3po <>
2018-01-04 10:22:01 +0100

    TP 01 - Les fonctions
    Définition d’une fonction

--- 01-rappels-es5/01.function.js
+++ 01-rappels-es5/01.function.js
@@ -1,1 +1,12 @@
 console.log("01 - Fonctions");
+
+nombre1 = 10;
+nombre2 = 20;
+
+function additionner(nb1, nb2){
+	return nb1 + nb2
+}
+
+resultat1 = additionner(nombre1, nombre2) ;
+
+console.log("resultat1 = " + resultat1);
