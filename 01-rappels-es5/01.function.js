53976002ef23f95ab1bed75a5092f65842913f99
myR3po <>
2018-01-04 10:29:39 +0100

    TP 01 - Les fonctions
    Variable de type fonction

--- 01-rappels-es5/01.function.js
+++ 01-rappels-es5/01.function.js
@@ -7,6 +7,17 @@ function additionner(nb1, nb2){
 	return nb1 + nb2
 }
 
-resultat1 = additionner(nombre1, nombre2) ;
-
+resultat1 = additionner(nombre1, nombre2);
 console.log("resultat1 = " + resultat1);
+
+somme = additionner;
+
+resultat2 = somme(nombre1, nombre2);
+console.log("resultat2 = " + resultat2);
+
+
+multiplication = function(nb1, nb2){
+					return nb1 * nb2
+				}
+resultat3 = multiplication(nombre1, nombre2);
+console.log("resultat3 = " + resultat3);
