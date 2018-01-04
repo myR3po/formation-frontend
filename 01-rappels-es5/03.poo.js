3f6711bbea978a701058c7fa4efc6b1990a3bb06
myR3po <>
2018-01-04 12:27:01 +0100

    TP 03 - Programmation orientée objet
    Objet sans fonction constructeur

--- 01-rappels-es5/03.poo.js
+++ 01-rappels-es5/03.poo.js
@@ -5,7 +5,7 @@ function Personne(nom, prenom, pseudo){
 	self.pseudo = pseudo
 	
 	this.getNomComplet = function(){
-		return nom + " " + prenom + " " + pseudo
+		return this.nom + " " + this.prenom + " [ " + this.pseudo + " ]"
 	}
 	
 	this.getInitiales = function(){
@@ -17,6 +17,7 @@ afficherPersonne = function(personne){
 	console.log('Le nom : ' + personne.nom)
 	console.log('Le prenom : ' + personne.prenom)
 	console.log('Le pseudo : ' + personne.pseudo)
+	console.log('Le nom complet : ' + personne.getNomComplet())
 	console.log('\n')
 }
 
@@ -35,4 +36,15 @@ console.log(jules.age)
 jules.age = 30
 console.log(jules.age)
 
-console.log(jules.getInitiales())
+console.log(jules.getInitiales(), '\n')
+
+robert = {
+	prenom : "Robert",
+	nom : "LEPREFET", 
+	pseudo : "robert77", 
+	getNomComplet : function(){
+		return this.nom + " " + this.prenom + " [ " + this.pseudo + " ]"
+	}
+}
+
+afficherPersonne(robert)
