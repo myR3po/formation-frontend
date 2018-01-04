cf287c8d175584c653f887daa0e94790af60f5ff
myR3po <>
2018-01-04 10:44:40 +0100

    TP 01 - Les fonctions
    Fonction comme élément du 1er ordre

--- 01-rappels-es5/01.function.js
+++ 01-rappels-es5/01.function.js
@@ -21,3 +21,16 @@ multiplication = function(nb1, nb2){
 				}
 resultat3 = multiplication(nombre1, nombre2);
 console.log("resultat3 = " + resultat3);
+
+afficherOperation = function(nomOperation, operation, nb1, nb2){
+	return nomOperation + "  [ nb1 == " + nb1 + " nb2 == " + nb2 + " ] == " + operation(nb1, nb2)
+}
+console.log(afficherOperation("Somme", somme, 20, 40));
+
+console.log(afficherOperation("Multiplication", multiplication, 10, 20));
+
+soustraction = function(nb1, nb2){
+					return nb1 - nb2
+				}
+
+console.log(afficherOperation("Soustraction", soustraction, 15, 5));
