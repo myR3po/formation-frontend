1c0f2ee1ebd3c4a2d2b3c40b99652ba08f2b51fd
myR3po <>
2018-01-04 12:02:27 +0100

    TP 03 - Programmation orientée objet
    Fonction constructeur

---
+++ 01-rappels-es5/03.poo.js
@@ -0,0 +1,23 @@
+function Personne(nom, prenom, pseudo){
+	self = this
+	self.nom = nom
+	self.prenom = prenom
+	self.pseudo = pseudo
+	
+	this.getNomComplet = function(){
+		return self.nom + " " + self.prenom + " " + self.pseudo
+	}
+}
+
+afficherPersonne = function(personne){
+	console.log('Le nom : ' + personne.nom)
+	console.log('Le prenom : ' + personne.prenom)
+	console.log('Le pseudo : ' + personne.pseudo)
+	console.log('\n')
+}
+
+jules = new Personne("LEMAIRE", "Jules", "jules77")
+afficherPersonne(jules)
+
+paul = new Personne("LEMAIRE", "Paul", "paul44")
+afficherPersonne(paul)
