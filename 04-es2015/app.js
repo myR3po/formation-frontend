65be00bcb435551d436dee5f3169386b1e32f9ff
myR3po <>
2018-01-05 12:21:29 +0100

    ES2015 let et const

---
+++ 04-es2015/app.js
@@ -0,0 +1,19 @@
+
+let favoriteCityId = "rome"
+console.log(favoriteCityId)
+
+favoriteCityId = "paris"
+console.log(favoriteCityId)
+
+
+// • Affecter une nouvelle valeur à la constante citiesId : un tableau vide par exemple. Que
+// constatez-vous dans la console ? Mettre en commentaire la ligne de code de la nouvelle
+// affectation.
+// • Ajouter un nouvel élément au tableau : "tokyo".
+// • Afficher dans la console (console.log(...)) la constante citiesId.
+
+const citiesId = ["paris", "nyc", "rome", "rio-de-janeiro"]
+console.log(citiesId)
+// citiesId = []
+citiesId.push("tokyo")
+console.log(citiesId)
