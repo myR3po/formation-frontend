10e52c47bb33557ec58d8d7c8bf7ef5507b9b31d
myR3po <>
2018-01-05 12:32:41 +0100

    ES2015 Création d’objet

--- 04-es2015/app.js
+++ 04-es2015/app.js
@@ -1,19 +1,25 @@
-
+console.log("*** Utilisation de `let` ***")
 let favoriteCityId = "rome"
 console.log(favoriteCityId)
 
 favoriteCityId = "paris"
 console.log(favoriteCityId)
 
-
-// • Affecter une nouvelle valeur à la constante citiesId : un tableau vide par exemple. Que
-// constatez-vous dans la console ? Mettre en commentaire la ligne de code de la nouvelle
-// affectation.
-// • Ajouter un nouvel élément au tableau : "tokyo".
-// • Afficher dans la console (console.log(...)) la constante citiesId.
-
+console.log("\n\n*** Utilisation de `const` ***")
 const citiesId = ["paris", "nyc", "rome", "rio-de-janeiro"]
 console.log(citiesId)
 // citiesId = []
 citiesId.push("tokyo")
 console.log(citiesId)
+
+console.log("\n\n*** `Création d’objet` ***")
+function getWeather(cityId){
+	let city = cityId.toUpperCase()
+	let temperature = 20
+	
+	return {city, temperature}
+}
+
+const weather = getWeather("tokyo")
+console.log("Object",weather)
+
